94a83892e0e163425b6dfbcad59d5af84198eea4
Buce-Ithon <78635197+Buce-Ithon@users.noreply.github.com>
2024-10-24 17:05:42 +0800

    Initial commit

---
+++ src/main/java/net/hyprnoob/butterfly/ButterflyClient.java
@@ -0,0 +1,10 @@
+package net.hyprnoob.butterfly;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ButterflyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
